8c1cce123b6117eb27f19564257908f29e02b0e3
Kujanenj <44529387+Kujanenj@users.noreply.github.com>
2020-06-29 11:45:23 +0300

    Parser updates

--- Project/app/src/main/java/sini/foxy/furscanner/Parser.kt
+++ Project/app/src/main/java/sini/foxy/furscanner/Parser.kt
@@ -13,8 +13,39 @@ class Parser(){
 
         return true
     }
+    /*
+    NOTE: Add some safetyGuards, and the test all!
+     */
     public fun Parse(barcode : String) : IdPair {
-        TODO("not done")
+       println("----Parse start---")
+
+
+        var houseIDSubstring = barcode.substring(0,6)
+        var foxIDSubstring = barcode.substring(7,15) //
+        var indexToRemove = 0
+
+
+        for (i in 0..houseIDSubstring.length){
+            if(houseIDSubstring[i]!='0'){
+                break
+            }
+            indexToRemove++
+        }
+        houseIDSubstring=houseIDSubstring.drop(indexToRemove)
+
+
+        for (i in 0..foxIDSubstring.length){
+            if(foxIDSubstring[]!='0'){
+                break
+            }
+            indexToRemove++
+        }
+        foxIDSubstring=foxIDSubstring.drop(indexToRemove)
+        foxIDSubstring=foxIDSubstring.dropLast(1)
+
+        println("---Parse complete----")
+        return IdPair(houseIDSubstring,foxIDSubstring)
+
     }
 
 }
