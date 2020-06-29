3494a02ce2cc4041cbeb2b3ee6a67822d0a53749
Kujanenj <44529387+Kujanenj@users.noreply.github.com>
2020-06-29 12:05:47 +0300

    Parser safetyGuard added

--- Project/app/src/main/java/sini/foxy/furscanner/Parser.kt
+++ Project/app/src/main/java/sini/foxy/furscanner/Parser.kt
@@ -1,10 +1,10 @@
 package sini.foxy.furscanner
 
-
+import java.lang.Exception
 
 
 class Parser(){
-    private var activeMode = Modes.NO_MODE
+   /* private var activeMode = Modes.NO_MODE
 
 
     public fun setMode(newMode : Modes ) : Boolean{
@@ -13,15 +13,22 @@ class Parser(){
 
         return true
     }
+
+    */
+
     /*
     NOTE: Add some safetyGuards, and the test all!
      */
     public fun Parse(barcode : String) : IdPair {
        println("----Parse start---")
 
+        if(barcode.length!=16){
+            throw Exception("Wrong length barcode in parser: " + barcode.length +" chars")
+        }
 
-        var houseIDSubstring = barcode.substring(0,6)
-        var foxIDSubstring = barcode.substring(7,15) //
+
+        var houseIDSubstring = barcode.substring(0,7)
+        var foxIDSubstring = barcode.substring(7,16)
         var indexToRemove = 0
 
 
@@ -31,17 +38,21 @@ class Parser(){
             }
             indexToRemove++
         }
+
         houseIDSubstring=houseIDSubstring.drop(indexToRemove)
 
-
+        indexToRemove=0
         for (i in 0..foxIDSubstring.length){
-            if(foxIDSubstring[]!='0'){
+            if(foxIDSubstring[i]!='0'){
                 break
             }
             indexToRemove++
         }
+
         foxIDSubstring=foxIDSubstring.drop(indexToRemove)
+
         foxIDSubstring=foxIDSubstring.dropLast(1)
+
 
         println("---Parse complete----")
         return IdPair(houseIDSubstring,foxIDSubstring)
