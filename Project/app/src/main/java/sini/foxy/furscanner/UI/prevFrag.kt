c9d2201be7492f2fb1bed8d534fa9e7fb0642d08
Kujanenj <44529387+Kujanenj@users.noreply.github.com>
2020-07-23 12:38:36 +0300

    Add: Simple recyclerView

--- Project/app/src/main/java/sini/foxy/furscanner/UI/prevFrag.kt
+++ Project/app/src/main/java/sini/foxy/furscanner/UI/prevFrag.kt
@@ -1,10 +1,14 @@
 package sini.foxy.furscanner.UI
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import sini.foxy.furscanner.IdPair
 
 import sini.foxy.furscanner.R
 
@@ -18,7 +22,12 @@ class prevFrag : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_prev, container, false)
+        val view = inflater.inflate(R.layout.fragment_prev, container, false)
+        val recycler  = view.findViewById<View>(R.id.testRecyclerView) as RecyclerView
+        val adapter = TestRecyclerAdapter(listOf(IdPair("13","13"),IdPair("14","14")))
+        recycler.adapter = adapter
+        recycler.layoutManager = LinearLayoutManager(activity)
+        return view
     }
 
 }
