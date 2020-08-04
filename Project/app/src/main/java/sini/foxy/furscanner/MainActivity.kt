d7f7d2e438bcfbc0f3085b79de319e90f6fcfc67
Kujanenj <44529387+Kujanenj@users.noreply.github.com>
2020-08-04 14:27:04 +0300

    WIP: DrawLayout

--- Project/app/src/main/java/sini/foxy/furscanner/MainActivity.kt
+++ Project/app/src/main/java/sini/foxy/furscanner/MainActivity.kt
@@ -1,12 +1,18 @@
 package sini.foxy.furscanner
 
 import android.content.Context
+import android.content.res.Configuration
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.util.AttributeSet
+import android.view.MenuItem
 import android.view.View
 
+import androidx.appcompat.app.ActionBarDrawerToggle
+
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.viewpager.widget.ViewPager
 import com.google.android.material.tabs.TabLayout
 import sini.foxy.furscanner.Controller.Controller
@@ -19,6 +25,11 @@ import sini.foxy.furscanner.model.RandomGenerator
 //TODO : replace all test stuff with actual implementations
 //DESKTOP-06IBLMI
 class MainActivity : AppCompatActivity(), OnDataPass{
+
+    private lateinit var drawer: DrawerLayout
+    private lateinit var toggle: ActionBarDrawerToggle
+
+
 
     private var controller = Controller()
     override fun onDataPass(data : Pair<String,String>) {
@@ -43,8 +54,32 @@ class MainActivity : AppCompatActivity(), OnDataPass{
         testViewPager.adapter = fragmentAdapter
         testTabLayout.setupWithViewPager(testViewPager)
 
+        val navController =
+        val toolbar: Toolbar = findViewById(R.id.toolbar_main)
+      //  setSupportActionBar(toolbar)
+        drawer = findViewById(R.id.drawer_layout)
+
+        toggle = ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer.addDrawerListener(toggle)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
+    }
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        toggle.syncState()
     }
 
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        toggle.onConfigurationChanged(newConfig)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
 
     }
 
